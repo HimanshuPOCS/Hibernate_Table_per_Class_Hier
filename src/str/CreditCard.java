c173897834aaaa5ae021033e2b02883b5d9bde41
HimanshuPOCS <himanshusyntel@gmail.com>
2016-07-08 18:06:41 -0400

    Hibernate Inheritance: Table Per Class Hierarchy http://www.java4s.com/hibernate/hibernate-inheritance-table-per-class-hierarchy/

---
+++ src/str/CreditCard.java
@@ -0,0 +1,17 @@
+package str;
+
+public class CreditCard extends Payment{
+
+	private String CreditCardType;
+	
+
+	public String getCreditCardType() {
+		return CreditCardType;
+	}
+
+	public void setCreditCardType(String creditCardType) {
+		CreditCardType = creditCardType;
+	}
+	
+		
+}
